66e017297c67e604cf73e89f36ed7859d50ba1d4
YK <3314152137@qq.com>
2022-03-30 10:11:12 +0800

    修复跳转链接

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,9 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+     
+     //注册成功跳转网址
+    public const HOME = '/';
 
     /**
      * The controller namespace for the application.
