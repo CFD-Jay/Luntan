6bd6df65a1f910b3daf5100b0d95116de1bebc01
YK <3314152137@qq.com>
2022-04-01 11:22:29 +0800

    限制头像分辨率

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -29,6 +29,7 @@ class UserRequest extends FormRequest
             'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),//名字在users表是独一无二的，（除了自己）.
             'email' => 'required|email',
             'introduction' => 'max:80',
+            'avatar'=>'mimes:jpg,png,jpeg,gif|dimensions:min_weight=208,min_height=208',
         ];
     }
     
@@ -40,6 +41,8 @@ class UserRequest extends FormRequest
             'name.regex' => '用户名只支持英文、数字、横杠和下划线。',
             'name.between' => '用户名必须介于 3 - 25 个字符之间。',
             'name.required' => '用户名不能为空。',
+            'avatar.mimes' =>'头像必须是 png, jpg, gif, jpeg 格式的图片',
+            'avatar.dimensions' => '图片的清晰度不够，宽和高需要 208px 以上',
         ];
     }
 }
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -20,7 +20,7 @@
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
               aria-haspopup="true" aria-expanded="false">
-              <img src="{{$user->avatar}}" width="30px" height="30px">
+              <img src="{{Auth::user()->avatar}}" width="30px" height="30px">
               {{ Auth::user()->name }}
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
