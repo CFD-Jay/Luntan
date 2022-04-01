861dabf8b8290b0fd96516ba46300fff0e90c314
YK <3314152137@qq.com>
2022-04-01 16:21:01 +0800

    生成话题骨架

--- database/migrations/2022_04_01_122018_seed_categories_data.php
+++ database/migrations/2022_04_01_122018_seed_categories_data.php
@@ -5,6 +5,13 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 
+// 我们想要 LaraBBS 论坛软件在安装的时候，就初始化本文最前面提到的四个分类。
+
+// 面对数据库内容填充的需求，一般情况下我们会使用 Laravel 的 『数据填充 Seed』 。可是在当下场景中，我们无法使用此功能。此一般是用来生成假数据，而现在我们需要生成的是项目的 初始化数据，这些数据是项目运行的一部分，在生产环境下也会使用到，而数据填充只能在开发时使用。
+
+// 虽然 Laravel 没有自带此类解决方案，不过数据迁移功能倒是比较不错的替代方案。在功能定位上，数据迁移也是项目的一部分，执行的时机刚好是在项目安装时。并且区分执行先后顺序，这确保了初始化数据发生在数据表结构创建完成后。
+
+// 接下来我们使用命令生成数据迁移文件，作为 初始化数据 的迁移文件，我们定义命名规范为 seed_(数据库表名称)_data ：
 return new class extends Migration
 {
     public function up()
